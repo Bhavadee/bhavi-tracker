b383135281ab9044a8300b599224747b03308d8b
bhavadeep reddy <bhavadeep2003@gmail.com>
2022-08-22 11:38:58 -0700

    commit

--- index.js
+++ index.js
@@ -33,11 +33,11 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
-deleteBtn.addEventListener("dblclick", function() {
+deleteBtn.addEventListener( "dblclick", function() {
     localStorage.clear()
     myLeads = []
     render(myLeads)
-})
+ })
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
